83112d480f995fc799327597b78a11ab3fa34be5
Robson Narcizo <rob.narcizo.27@gmail.com>
2023-12-25 15:34:20 -0300

    refactor: Refatoração dos Parâmetros de Configurações Iniciais

--- php/app/source/Core/Config.php
+++ php/app/source/Core/Config.php
@@ -44,3 +44,19 @@ define('CONF_BASE_URL', 'http://localhost:8000'); # Produção
   define('CONF_IMG_ALLOW_TYPES', ['image/jpg', 'image/png', 'image/jpeg', 'image/webp']);
   define('CONF_IMG_UPLOAD_FOLDER_PATH', CONF_UPLOADS_FOLDER.'images/');
 
+  /* DATABASE */
+  define('DATA_LAYER_CONFIG', [
+    "driver" => $_ENV['MYSQL_DRIVER'],
+    "host" => $_ENV['MYSQL_HOST'],
+    "port" => $_ENV['MYSQL_PORT'],
+    "dbname" => $_ENV['MYSQL_DBNAME'],
+    "username" => $_ENV['MYSQL_USER'],
+    "passwd" => $_ENV['MYSQL_PASSWORD'],
+    "options" => [
+        PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8",
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ,
+        PDO::ATTR_CASE => PDO::CASE_NATURAL
+    ]
+  ]);
+
