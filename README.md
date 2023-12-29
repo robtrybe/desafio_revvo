8a35770f67be2299589454de4b3f941afa2f7a25
Robson Narcizo <rob.narcizo.27@gmail.com>
2023-12-29 12:12:25 -0300

    doc: Informações do Desafio Proposto

---
+++ README.md
@@ -0,0 +1,48 @@
+# Desafio Revvo
+
+
+## Informações do Desenvolvedor
+
+`Nome:`  Robson de Jesus Narcizo
+`Email:` rob.narcizo.27@gmail.com
+`Celular` (16) 99145-9236
+
+## Sobre o desafio proposto
+
+A aplicação foi construida utilizando `Docker`. Por esse motivo é necessário ter o `Docker` junto com 
+`Docker Compose`. A mesma é acessada através da url `http://localhost:8000`.Antes de acessar é necessário
+executar o seguinte comando no terminal:
+
+```bash
+    docker-compose up -d
+```
+
+`CRUD`
+
+`OBS:` O `CRUD` é feito atrvés da técnica de `Form Spoofing` utilizando o método `POST` para 
+acesso aos outros verbos.
+
+A primeira `/admin` ou `Dashboard` via método `GET` onde é possivel ver todos os cursos nos seus respectivos cards. Através da dash é possivel `acessar` ou `editar` os cursos.
+
+A sugunda `/admin/course/id_do_curso/edit` para edição de um curso via método `POST`. Na aplicação a edição
+de um curso é feita utilizando a mesma rota porém via método `GET`.
+
+A terceira `/admin/course/id_do_curso` para remoção de um curso via método `POST`. Na aplicação, a exclusão
+de um curso pode ser feito através da rota de edição.
+
+A rota `/course/id_curso` via método `GET` é utilizada para visualização de um curso específico tanto.A imagem contida em cada curso é responsiva se adaptando a diferentes resoluções poupando recursos do servidor.
+
+## Web
+
+A parte `Web` da aplicação possui duas rotas
+
+A primeira `/` para renderização da `Página Home`
+
+A segunda `/course/{id}` para visualização de um `Curso específico`.
+
+`OBS:` A parte `Web` da aplicação encontra-se estática no momento, porém responsiva. O preenchimento dos
+cursos foram feito utilizando `estrutura de repetição`, porém a página se adapta a dispositivos moveis, ou 
+seja , utilizando `Mobile First`. Infelizmente devido ao tempo não foi possível finalizar a alimentação por completo.
+
+
+
--- database/database.sql
+++ database/database.sql
@@ -6,10 +6,30 @@ CREATE TABLE IF NOT EXISTS courses(
     id int unsigned not null auto_increment,
     name varchar(100) not null,
     slug varchar(100) not null,
-    slide_image varchar(100) not null,
+    slide_image varchar(100) default NULL,
     description TEXT not null,
-    cover_image varchar(100) not null,
+    cover_image varchar(100) default NULL,
     created_at timestamp DEFAULT CURRENT_TIMESTAMP,
     updated_at timestamp DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     primary key(id)
 ) Engine=InnoDB CHARACTER SET utf8 COLLATE utf8_general_ci;
+
+insert into courses(name, slug, description) values('Javascript', 'javascript', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur rutrum purus feugiat, auctor nunc ac, faucibus libero. Fusce sodales consequat lorem eu eleifend. Nam consequat');
+insert into courses(name, slug, description) values('PhP', 'php', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur rutrum purus feugiat, auctor nunc ac, faucibus libero. Fusce sodales consequat lorem eu eleifend. Nam consequat');
+insert into courses(name, slug, description) values('C Sharp', 'c-sharp', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur rutrum purus feugiat, auctor nunc ac, faucibus libero. Fusce sodales consequat lorem eu eleifend. Nam consequat');
+insert into courses(name, slug, description) values('Java', 'java', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur rutrum purus feugiat, auctor nunc ac, faucibus libero. Fusce sodales consequat lorem eu eleifend. Nam consequat');
+insert into courses(name, slug, description) values('Go Lang', 'go-lang', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur rutrum purus feugiat, auctor nunc ac, faucibus libero. Fusce sodales consequat lorem eu eleifend. Nam consequat');
+insert into courses(name, slug, description) values('C', 'c', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur rutrum purus feugiat, auctor nunc ac, faucibus libero. Fusce sodales consequat lorem eu eleifend. Nam consequat');
+insert into courses(name, slug, description) values('Delphi', 'delphi', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur rutrum purus feugiat, auctor nunc ac, faucibus libero. Fusce sodales consequat lorem eu eleifend. Nam consequat');
+CREATE TABLE IF NOT EXISTS users(
+    id int unsigned not null auto_increment,
+    first_name varchar(50) not null,
+    last_name varchar(50) not null,
+    email varchar(100) not null,
+    password varchar(255) not null,
+    primary key(id)
+) Engine=InnoDB CHARACTER SET utf8 COLLATE utf8_general_ci;
+
+insert into users(first_name, last_name, email, password) 
+    values('Carlos', 'Silva', 'carlos@gmail.com', '$2y$10$4DXoe.rx1O74QSGh2kDYfOeKk4n3XraNXyvhx7VMsLw2S/5KckGI6');
+
